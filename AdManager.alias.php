77bfc202269ea35c9c3d1e05f774aa80fd1f020c
Umherirrender <umherirrender_de.wp@web.de>
2018-09-20 22:02:49 +0200

    Use short array syntax in i18n alias file
    Change-Id: Iee470d6e963bc050b5eab7bc57c327a5f26c5cf3

--- AdManager.alias.php
+++ AdManager.alias.php
@@ -5,101 +5,100 @@
  * @file
  * @ingroup Extensions
  */
-// @codingStandardsIgnoreFile
 
-$specialPageAliases = array();
+$specialPageAliases = [];
 
 /** English (English) */
-$specialPageAliases['en'] = array(
-	'AdManager' => array( 'AdManager', 'Ad Manager' ),
-	'AdManagerZones' => array( 'AdManagerZones', 'Ad Manager Zones' ),
-);
+$specialPageAliases['en'] = [
+	'AdManager' => [ 'AdManager', 'Ad Manager' ],
+	'AdManagerZones' => [ 'AdManagerZones', 'Ad Manager Zones' ],
+];
 
 /** Arabic (العربية) */
-$specialPageAliases['ar'] = array(
-	'AdManager' => array( 'مدير_الإعلانات' ),
-	'AdManagerZones' => array( 'مناطق_مدير_الإعلانات' ),
-);
+$specialPageAliases['ar'] = [
+	'AdManager' => [ 'مدير_الإعلانات' ],
+	'AdManagerZones' => [ 'مناطق_مدير_الإعلانات' ],
+];
 
 /** Egyptian Arabic (مصرى) */
-$specialPageAliases['arz'] = array(
-	'AdManager' => array( 'مدير_الاعلانات' ),
-	'AdManagerZones' => array( 'مناطق_مدير_الاعلانات' ),
-);
+$specialPageAliases['arz'] = [
+	'AdManager' => [ 'مدير_الاعلانات' ],
+	'AdManagerZones' => [ 'مناطق_مدير_الاعلانات' ],
+];
 
 /** Western Balochi (بلوچی رخشانی) */
-$specialPageAliases['bgn'] = array(
-	'AdManager' => array( 'تبلیغات_ئی_مدیر' ),
-	'AdManagerZones' => array( 'تبلیغات_ئی_مدیر_ئی_زون' ),
-);
+$specialPageAliases['bgn'] = [
+	'AdManager' => [ 'تبلیغات_ئی_مدیر' ],
+	'AdManagerZones' => [ 'تبلیغات_ئی_مدیر_ئی_زون' ],
+];
 
 /** German (Deutsch) */
-$specialPageAliases['de'] = array(
-	'AdManager' => array( 'Anzeigen_verwalten' ),
-	'AdManagerZones' => array( 'Anzeigenverwaltungsbereiche' ),
-);
+$specialPageAliases['de'] = [
+	'AdManager' => [ 'Anzeigen_verwalten' ],
+	'AdManagerZones' => [ 'Anzeigenverwaltungsbereiche' ],
+];
 
 /** Persian (فارسی) */
-$specialPageAliases['fa'] = array(
-	'AdManager' => array( 'مدیریت_تبلیغات' ),
-	'AdManagerZones' => array( 'نقاط_مدیریت_تبلیغات' ),
-);
+$specialPageAliases['fa'] = [
+	'AdManager' => [ 'مدیریت_تبلیغات' ],
+	'AdManagerZones' => [ 'نقاط_مدیریت_تبلیغات' ],
+];
 
 /** Hebrew (עברית) */
-$specialPageAliases['he'] = array(
-	'AdManager' => array( 'מנהל_פרסומות' ),
-	'AdManagerZones' => array( 'אזורי_מנהל_פרסומות' ),
-);
+$specialPageAliases['he'] = [
+	'AdManager' => [ 'מנהל_פרסומות' ],
+	'AdManagerZones' => [ 'אזורי_מנהל_פרסומות' ],
+];
 
 /** Korean (한국어) */
-$specialPageAliases['ko'] = array(
-	'AdManager' => array( '광고관리자' ),
-	'AdManagerZones' => array( '광고관리자영역' ),
-);
+$specialPageAliases['ko'] = [
+	'AdManager' => [ '광고관리자' ],
+	'AdManagerZones' => [ '광고관리자영역' ],
+];
 
 /** Northern Luri (لۊری شومالی) */
-$specialPageAliases['lrc'] = array(
-	'AdManager' => array( 'ئضاف_کئردئن_دیڤوٙندار' ),
-	'AdManagerZones' => array( 'ئضاف_کئردئن_راساگە_یا_دیڤوٙنداری' ),
-);
+$specialPageAliases['lrc'] = [
+	'AdManager' => [ 'ئضاف_کئردئن_دیڤوٙندار' ],
+	'AdManagerZones' => [ 'ئضاف_کئردئن_راساگە_یا_دیڤوٙنداری' ],
+];
 
 /** Malagasy (Malagasy) */
-$specialPageAliases['mg'] = array(
-	'AdManager' => array( 'Mpikarakara_doka', 'MpikarakaraDoka' ),
-);
+$specialPageAliases['mg'] = [
+	'AdManager' => [ 'Mpikarakara_doka', 'MpikarakaraDoka' ],
+];
 
 /** Macedonian (македонски) */
-$specialPageAliases['mk'] = array(
-	'AdManager' => array( 'РаководителСоРеклами' ),
-	'AdManagerZones' => array( 'РаководителСоРекламиПодрачја' ),
-);
+$specialPageAliases['mk'] = [
+	'AdManager' => [ 'РаководителСоРеклами' ],
+	'AdManagerZones' => [ 'РаководителСоРекламиПодрачја' ],
+];
 
 /** Dutch (Nederlands) */
-$specialPageAliases['nl'] = array(
-	'AdManager' => array( 'Advertentiebeheer' ),
-	'AdManagerZones' => array( 'Advertentiebeheerzones' ),
-);
+$specialPageAliases['nl'] = [
+	'AdManager' => [ 'Advertentiebeheer' ],
+	'AdManagerZones' => [ 'Advertentiebeheerzones' ],
+];
 
 /** Brazilian Portuguese (português do Brasil) */
-$specialPageAliases['pt-br'] = array(
-	'AdManager' => array( 'Gerenciador_de_propaganda' ),
-	'AdManagerZones' => array( 'Gerenciador_de_zonas_de_propaganda' ),
-);
+$specialPageAliases['pt-br'] = [
+	'AdManager' => [ 'Gerenciador_de_propaganda' ],
+	'AdManagerZones' => [ 'Gerenciador_de_zonas_de_propaganda' ],
+];
 
 /** Vietnamese (Tiếng Việt) */
-$specialPageAliases['vi'] = array(
-	'AdManager' => array( 'Quản_lý_quảng_cáo', 'Quản_lí_quảng_cáo' ),
-	'AdManagerZones' => array( 'Đới_quản_lý_quảng_cáo', 'Đới_quản_lí_quảng_cáo' ),
-);
+$specialPageAliases['vi'] = [
+	'AdManager' => [ 'Quản_lý_quảng_cáo', 'Quản_lí_quảng_cáo' ],
+	'AdManagerZones' => [ 'Đới_quản_lý_quảng_cáo', 'Đới_quản_lí_quảng_cáo' ],
+];
 
 /** Simplified Chinese (中文（简体）‎) */
-$specialPageAliases['zh-hans'] = array(
-	'AdManager' => array( '广告管理器' ),
-	'AdManagerZones' => array( '广告管理区域' ),
-);
+$specialPageAliases['zh-hans'] = [
+	'AdManager' => [ '广告管理器' ],
+	'AdManagerZones' => [ '广告管理区域' ],
+];
 
 /** Traditional Chinese (中文（繁體）‎) */
-$specialPageAliases['zh-hant'] = array(
-	'AdManager' => array( '廣告管理' ),
-	'AdManagerZones' => array( '廣告管理區域' ),
-);
+$specialPageAliases['zh-hant'] = [
+	'AdManager' => [ '廣告管理' ],
+	'AdManagerZones' => [ '廣告管理區域' ],
+];
